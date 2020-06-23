4276246b9b452b2e370a2af66473d7131c090ed2
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-06-23 23:19:25 +0400

    Finished nested routing

--- src/App/Home/Sightseeing/TableWrapper/SelectedItem/Item/Item.js
+++ src/App/Home/Sightseeing/TableWrapper/SelectedItem/Item/Item.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./Item.css";
 import { Container, Row, Col } from 'reactstrap';
-import { Nav, NavItem, NavLink } from 'reactstrap';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import iconSite from "./icon/internet.png";
 import iconFacebook from "./icon/facebook.png";
 import iconAddress from "./icon/address.png";
@@ -14,9 +13,11 @@ export function Item(props) {
     let { section, key } = useParams();
     let source = `Images/${section}/${props.name}`;
     let imgArr = [1,2,3,4,5,6,7,8,9,10,11,12];
+
     const handleClick=(e)=>{
         e.preventDefault();
         console.log("Hello")
+
     };
     return(
         <Container>
@@ -29,7 +30,6 @@ export function Item(props) {
                     {console.log(source)}
                 </Col>
                 <Col xs="12" sm="10" md="6" lg="6" xl="6" className={"item"}>
-
                     <div className={"imageNav"}>
                         {imgArr.map(x=>
                             <img onClick={handleClick} key={x} src={require(`./${source}/${x}.jpg`)}/>
@@ -44,7 +44,6 @@ export function Item(props) {
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d465883.9503245908!2d40.1618351!3d44.2889266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406aca1c0c3d6bc3%3A0xfbfcc202cc1fcfbc!2sHnaberd%2C+Armenia!5e0!3m2!1sen!2s!4v1525446123836"
                     width="100%" height="400" frameBorder="0" style={{border:"0"}} allowFullScreen="">
                     </iframe>
-
                 </Col>
             </Row>
             <Row className={"contacts"}>
@@ -89,9 +88,9 @@ export function Item(props) {
                                 <tbody>
                                 <tr>
                                     {   props.workTime &&
-                                    props.workTime.map((x,i)=>
-                                        <td key={i}>{x}</td>
-                                    )
+                                        props.workTime.map((x,i)=>
+                                            <td key={i}>{x}</td>
+                                        )
                                     }
                                 </tr>
                                 </tbody>
