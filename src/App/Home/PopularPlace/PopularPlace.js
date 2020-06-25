29970a5ba7687c31f3e6d1e4453cfd0a35a939dd
Mane45 <sofyan.mane@gmail.com>
2020-06-26 00:08:26 +0400

    modal+slider

--- src/App/Home/PopularPlace/PopularPlace.js
+++ src/App/Home/PopularPlace/PopularPlace.js
@@ -1,20 +1,12 @@
 import React from "react";
 import "./PopularPlace.css";
-import CoverFlow from 'coverflow-react';
-
-const imagesArr = [
-    'img/1.jpg',
-    'img/2.jpg',
-    'img/3.jpg',
-    'img/4.jpg',
-    'img/5.jpg',
-];
+import MutipleSlidesPerView from './PopularTourCardSlider';
 
 export default function PopularPlace(props) {
     return(
         <div className={"popular"}>
             <h2 className={"title"}>Ամենահայտնի տեղերը</h2>
-            <CoverFlow imagesArr={imagesArr} />
+            <MutipleSlidesPerView/>
         </div>
     )
 }
