46d25f7248bc45de79a29404b318e27ee1db19c3
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-07-02 19:02:33 +0400

    winehouse json

--- src/App/Home/Auth/Login/Login.js
+++ src/App/Home/Auth/Login/Login.js
@@ -31,19 +31,28 @@ export const Login = (props) => {
         <div className={"login"}>
             <h2 className={"title"}>Մուտք գործել</h2>
             <form onSubmit={handleLogin}>
-                <div className="row">
-                    <div className="col-75">
-                        <input className={"loginInput"} type="email" name="Email" placeholder="Enter your email"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-75">
-                        <input className={"loginInput"} type="password" name="pssword" placeholder="Enter your password"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <input className={"loginBtn"} type="submit" value="Մուտք"/>
-                </div>
+                <label>
+                    Email
+                    <input name="email" type="email" placeholder="Email" />
+                </label>
+                <label>
+                    Password
+                    <input name="password" type="password" placeholder="Password" />
+                </label>
+                <button type="submit">Log in</button>
+                {/*<div className="row">*/}
+                    {/*<div className="col-75">*/}
+                        {/*<input className={"loginInput"} type="email" name="Email" placeholder="Enter your email"/>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
+                {/*<div className="row">*/}
+                    {/*<div className="col-75">*/}
+                        {/*<input className={"loginInput"} type="password" name="pssword" placeholder="Enter your password"/>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
+                {/*<div className="row">*/}
+                    {/*<input className={"loginBtn"} type="submit" value="Մուտք"/>*/}
+                {/*</div>*/}
             </form>
         </div>
     );
