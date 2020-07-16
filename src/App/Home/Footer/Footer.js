a3c395ef51066232342263c360bdfa6462a7daff
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-07-16 06:52:21 +0400

    popular finished

--- src/App/Home/Footer/Footer.js
+++ src/App/Home/Footer/Footer.js
@@ -6,7 +6,7 @@ function Footer(props) {
     return (
         <div className="footer">
             <p >Copyright &copy; 2020 Armenian trip</p>
-            <p className={"nameCopyright"}>Lecture: <b>A.Margaryan</b>. Students: <b>M</b>. Sofyan, <b>V</b>. Hakobyan, <b>M</b>. Grigoryan</p>
+            <p className={"nameCopyright"}>Lecturer: <b>A.Margaryan</b>. Students: <b>M</b>. Sofyan, <b>V</b>. Hakobyan, <b>M</b>. Grigoryan</p>
         </div>
     )
 }
